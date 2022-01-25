54199f171bdc8a163e812321aeaa6620507fbccf
赤川大空 <daisuke1024akagawa@gmail.com>
2022-01-25 20:21:20 +0900

    Update version

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ def _get_requires_from_file(filename):
 
 setup(
     name="PyTaskManager",
-    version="1.0.0",
+    version="1.0.1",
     license="MIT",
     author="Akagawa Daisuke",
-    url="http://github.com/Akasan/PythonTaskManager",
+    url="http://github.com/Akasan/PyTaskManager",
     packages=["PyTaskManager"],
     include_package_data=True,
     zip_safe=False,
